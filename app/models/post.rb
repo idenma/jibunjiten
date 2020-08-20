96c0cdc4dc6cfe0edc55d8aa6e16b85d97724ca3
ig <ig@rutsukawasakinoiMac-4.local>
2020-08-20 14:59:19 +0900

    updated_at check

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ def self.search(search,p)
   if search && p
    contents.map do |content|#contentsに入ってる要素で検索
    all_post << Post.find_by_sql(
-     ["SELECT posts.id,posts.dictionary_id,heading,subheading,content_1,content_2 FROM posts join dictionaries on posts.dictionary_id = dictionaries.id
+     ["SELECT posts.id,posts.dictionary_id,heading,subheading,content_1,content_2,posts.updated_at FROM posts join dictionaries on posts.dictionary_id = dictionaries.id
        where user_id = ? AND "+ content +" Like ? ORDER BY dictionaries.id ASC",p,"%#{search}%"]
      )
   end
