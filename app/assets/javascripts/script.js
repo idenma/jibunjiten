cbb1ec8cc84b687a85fab5c2934c1a7d30817f95
ig <ig@rutsukawasakinoiMac-4.local>
2020-09-11 10:18:42 +0900

    css

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -4,63 +4,16 @@ $(function(){
   $('.dictionary-top').offset({top: 0,left: 0});
 
 
-
-
-//  タブを並べる
- var tag = $('.hidden').find('h4').text();//@Dictionaryを読み込む：選択している辞書
- var  choiseTag = 0;
- var tabNumber = $('.dictionary-single').length;
- var dictionarySingleTop = $('.dictionary-single').offset().top;
- var dictionarySingleLeft = $('.dictionary-single').offset().left;
- var choiseTagDepth = new Array(tabNumber);
- var tagStyle = new Array(tabNumber);
-
-
- for (var i = 0;i < tabNumber; i++) {
-  var lineUpTag = $('.dictionary-single').eq(i).find('h6').text();
-   tagStyle[i] = $('.dictionary-single').eq(i).find('h5').text();
-  if ( lineUpTag == tag) {
-      choiseTag += 10;
-    }else {
-      choiseTag = 0;
-    }
-    choiseTagDepth[i] = dictionarySingleTop + choiseTag;
-   $('.dictionary-single').eq(i).offset({top: choiseTagDepth[i],left: dictionarySingleLeft + (i * 155)});
-
-  if (tagStyle[i] == "1"){
-      $('.dictionary-single').eq(i).css('background-color','#dddde4');
-    } else
-    if (tagStyle[i] == "0"){
-      $('.dictionary-single').eq(i).css('background-color','#caafaf');
-    } else {
-      $('.dictionary-single').eq(i).css('background-color','#b0c4de');
-    }
-  }
-
-    $('.head-bar').offset({top: dictionarySingleTop + 30,left: 0});
+//タブをブラウザ幅に合わせて動かす考え方
+//タブの幅　＊　タブの数で全タブの幅を計算
+//ブラウザの幅が全タブの幅を超えた時に
+//下の段に押しやる　
 
 
 
-//  タブを飛び上がらせる
-  $('.dictionary-single').hover(
-    function(){
 
 
-  $(this).animate({
-   top:"20px",
-   height:"40px"
-   },100);
-  },
-    function(){
 
-      var dictionarySingleEq = $('.dictionary-single').index($(this));
-      var choiseTagHeight = choiseTagDepth[dictionarySingleEq];
-      $(this).animate({
-        top:choiseTagHeight,
-        height:"30px"
-        },100);
-
-    });
 
 //  ログイン情報の表示
  $('.dictionary-heading').click(function(){
@@ -134,6 +87,77 @@ $(window).on('load resize', function() {
       searchFormClick = false
     }
    });
+
+
+
+//
+   //  タブを並べる
+    var rowTag = 0;//タグの列初期化
+    var tag = $('.hidden').find('h4').text();//@Dictionaryを読み込む：選択している辞書
+    var  choiseTag = 0;//選択しているタグ（辞書）用変数:初期化
+    var tabNumber = $('.dictionary-single').length;//タブの数を数える
+    var dictionarySingleTop = $('.dictionary-single').offset().top;//タブの縦位置を取得
+    var dictionarySingleLeft = $('.dictionary-single').offset().left;//タブの横位置を取得
+    var choiseTagDepth = new Array(tabNumber);//タブの数だけ
+    var tagStyle = new Array(tabNumber);//タグのスタイル取得用変数
+
+
+//  if (windowWidth < dictionarySingleLeft + (tabNumber * 155)) {//タブに行を作るためのif文
+//　/タブ数
+  //}
+//タブに行を作るためのfor文
+    for (var i = 0;i < tabNumber; i++) {//タブを一つずつ並べる作業
+     var lineUpTag = $('.dictionary-single').eq(i).find('h6').text();//
+      tagStyle[i] = $('.dictionary-single').eq(i).find('h5').text();//タグのスタイル取得用変数
+     if ( lineUpTag == tag) {//タグが選ばれたタグなら（選択中の辞書なら）
+         choiseTag += 10;//タグの位置　+10下に　（タグが押し込まれてるように表示）
+       }else {//選択されてないタグは
+         choiseTag = 0;//タグの位置　デフォルト
+       }
+       choiseTagDepth[i] = dictionarySingleTop + choiseTag;//
+      $('.dictionary-single').eq(i).offset({top: choiseTagDepth[i],left: dictionarySingleLeft + (i * 155)});//タグを並べている確信のコード
+
+     if (tagStyle[i] == "1"){//タグのスタイルが１だった場合
+         $('.dictionary-single').eq(i).css('background-color','#dddde4');//
+       } else
+       if (tagStyle[i] == "0"){//タグのスタイルが０だった場合
+         $('.dictionary-single').eq(i).css('background-color','#caafaf');//
+       } else {
+         $('.dictionary-single').eq(i).css('background-color','#b0c4de');//
+       }
+     }
+//タブに行を作るためのfor文終わり
+//}//タブに行を作るためのif文終わり
+
+       $('.head-bar').offset({top: dictionarySingleTop + 30,left: 0});//ここもタブ行ができれば変わる
+
+       //  タブを飛び上がらせる
+         $('.dictionary-single').hover(
+           function(){
+
+
+         $(this).animate({
+          top:"20px",
+          height:"40px"
+          },100);
+         },
+           function(){
+
+             var dictionarySingleEq = $('.dictionary-single').index($(this));
+             var choiseTagHeight = choiseTagDepth[dictionarySingleEq];
+             $(this).animate({
+               top:choiseTagHeight,
+               height:"30px"
+               },100);
+
+           });
+
+
+
+
+
+
+
 
 });//　ブラウザリサイズに影響するもの終わり
 　
